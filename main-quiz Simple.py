558168d47696cf7139362389f1e34c0b5401016a
karanBRAVO <77043443+karanBRAVO@users.noreply.github.com>
2021-01-21 13:47:35 +0530

    Add files via upload

---
+++ main-quiz Simple.py
@@ -0,0 +1,189 @@
+score = 0
+
+#QUESTION 1
+answer1 = input("1. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer1 == "b" or answer1 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 2
+answer2 = input("2. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer2 == "b" or answer2 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 3
+answer3 = input("3. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer3 == "b" or answer3 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#Question 4
+answer4 = input("4. 1mole= ? \na. 6.022x10^23 \nb. 1.23x10^-23 \nc. 6.02x106-23 \nd. 6022x10^23 \nAnswer: ")
+if answer4 == "a" or answer4 =="6.022x10^23":
+    score += 1
+    print("Correct!")
+    print("Score; ", score)
+    print("\n")
+else:
+    print("Incorrect! The correct answer is (a).")
+    print("Score: ", score)
+    print("\n")
+
+#QUESTION 5
+answer5 = input("5. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer5 == "b" or answer5 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 6
+answer6 = input("6. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer6 == "b" or answer6 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 7
+answer7 = input("7. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer7 == "b" or answer7 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 8
+answer8 = input("8. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer8 == "b" or answer8 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 9
+answer9 = input("9.What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer9 == "b" or answer9 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 10
+answer10 = input("10. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer10 == "b" or answer10 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 11
+answer11 = input("11. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer11 == "b" or answer11 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 12
+answer12 = input("12. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer12 == "b" or answer12 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 13
+answer13 = input("13. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer13 == "b" or answer13 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 14
+answer14 = input("14. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer14 == "b" or answer14 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+#QUESTION 15
+answer15 = input("15. What is the capital of QUEENSLAND? \na. Townville \nb. Brisbane \nc. Noosa \nAnswer: ")
+if answer15 == "b" or answer15 == "Brisbane":
+    score += 1
+    print("Correct!")
+    print("Score:", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Brisbane." )
+    print("Score:", score)
+    print("\n")
+
+# Final Message
+if score <= 10:
+    print("Your score is:", score, "- Average.")
+elif score <= 11:
+    print("Your scoer is:", score, "- Good, Can do better.")
+else:
+    print("Your scrore is:", score, "Excellent!!!")
